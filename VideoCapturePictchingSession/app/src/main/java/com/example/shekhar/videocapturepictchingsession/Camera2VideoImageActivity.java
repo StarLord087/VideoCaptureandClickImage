f68f28b7e81e31434a6f6f822ef4ff192e0ef7a9
Shekhar <shekahr.singh087@gmail.com>
2017-10-12 21:23:55 +0530

    Setting up TextureView

--- VideoCapturePictchingSession/app/src/main/java/com/example/shekhar/videocapturepictchingsession/Camera2VideoImageActivity.java
+++ VideoCapturePictchingSession/app/src/main/java/com/example/shekhar/videocapturepictchingsession/Camera2VideoImageActivity.java
@@ -1,15 +1,58 @@
 package com.example.shekhar.videocapturepictchingsession;
 
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
 
 public class Camera2VideoImageActivity extends AppCompatActivity {
+    private TextureView mTexturView;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            //to check if textureView is available
+            Toast.makeText(Camera2VideoImageActivity.this, "TextureView Available", Toast.LENGTH_SHORT).show();
+            
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        if(mTexturView.isAvailable()){
+
+        }
+        else {
+            mTexturView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2_video_image);
+
+
+        mTexturView = (TextureView) findViewById(R.id.textureView);
+
     }
 //Converting App to fullscreen  Sticky immersive mode
     @Override
