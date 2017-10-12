0b4b2ddfaf85fa925c4c1dc84d2ead879ea5bac6
Shekhar <shekahr.singh087@gmail.com>
2017-10-12 20:16:24 +0530

    Converting app to fullscreen immersive sticky mode

--- VideoCapturePictchingSession/app/src/main/java/com/example/shekhar/videocapturepictchingsession/Camera2VideoImageActivity.java
+++ VideoCapturePictchingSession/app/src/main/java/com/example/shekhar/videocapturepictchingsession/Camera2VideoImageActivity.java
@@ -2,6 +2,7 @@ package com.example.shekhar.videocapturepictchingsession;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Camera2VideoImageActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2_video_image);
+    }
+//Converting App to fullscreen  Sticky immersive mode
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        View decorView = getWindow().getDecorView();
+        if(hasFocus){
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            |View.SYSTEM_UI_FLAG_FULLSCREEN
+            |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+
     }
 }
